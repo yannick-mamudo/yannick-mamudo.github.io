997de56466c1b908375530fb09230b0575090f64
yannick-mamudo <yannick.mamudo@gmail.com>
2016-09-12 23:05:01 -0500

    Nav bar update on scroll - works

--- js/action.js
+++ js/action.js
@@ -1,26 +1,35 @@
 // Function for nav bar
 // Adapted from http://codepen.io/clokey2k/pen/jgfFD
-$(document).ready(function() {
-    alert("Hi there we in this.");
-    // $sections includes all of the page divs that relate to menu items.
-    var $sections = $('.page');
+$ = jQuery;
+
+// $sections includes all of the page divs that relate to menu items.
+var $sections = $('.page');
+
+$(function() {
+    navbarUpdater();
+
     // The user scrolls
-    $(window).scroll(function(event) {
-        alert("You scrolled homie. Bless up.")
-        var $currentSection;
-        var $minDistance = Number.POSITIVE_INFINITY;
-        $sections.each(function() {
-            // divDistance is the distance(in px) of the head of a div to the head of the view/window
-            var $divDistance = Math.abs($(this).offset().top);
-            if ($divDistance <= $minDistance) {
-                $currentSection = $(this);
-                $minDistance = $divDistance;
-            }
-        });
-        console.log("minDistance = " + $minDistance.toString());
-        // This is the bit of code that uses the currentSection as its source of ID
-        var id = $($currentSection).attr('id');
-        $('a').removeClass('active-nav');
-        $("[href='#" + id + "']").addClass('active-nav');
+    $('.docScroller').scroll(function() {
+        navbarUpdater();
     });
 });
+
+function navbarUpdater() {
+    var $currentSection;
+    var $minDistance = Number.POSITIVE_INFINITY;
+
+    $sections.each(function() {
+        // divDistance is the distance(in px) of the head of a div to the head of the view/window
+        var $divDistance = Math.abs($(this).offset().top);
+        console.log($divDistance.toString());
+        if ($divDistance <= $minDistance) {
+            $currentSection = $(this);
+            $minDistance = $divDistance;
+        }
+    });
+
+    // Removes .active-nav from all nav bar options then applies it to appropriate button
+    var id = $currentSection.attr('id');
+    $('a').removeClass('active-nav');
+    $("[href='#" + id + "']").addClass('active-nav');
+}
